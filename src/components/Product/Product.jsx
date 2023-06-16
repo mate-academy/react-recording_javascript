19d077729a52f78d63016a5541a997516ba0dd0c
Misha Hrynko <misha@mate.academy>
2023-06-16 11:24:22 +0300

    after Conditional Styling

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,9 +1,21 @@
+import cn from 'classnames';
+import './Product.scss';
+
 export const Product = ({
   title,
   price = 0,
 }) => (
-  <div className="Product">
-    <h2 className="Product__title">
+  // <div className={`Product ${price === 0 ? 'Product--free' : ''}`}>
+  <div className={cn('Product', {
+    'Product--free': price === 0,
+  })}>
+    <h2 
+      className="Product__title" 
+      style={{
+        color: price === 0 ? 'green' : 'red',
+        backgroundColor: 'yellow',
+      }}
+    >
       {title}
     </h2>
 
@@ -11,10 +23,7 @@ export const Product = ({
       {`Price: ${price} `}
 
       {price === 0 && (
-        <>
-          <span>*</span>
-          <span>*</span>
-        </>
+        <span>*</span>
       )}
     </div>
   </div>
